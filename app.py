51e32d2c85d4ab396a0238ef5890dcdd45649ffe
Analese Steverson Pugh <analese_steversonpugh@intuit.com>
2022-07-21 16:40:25 +0000

    updated app.py

--- app.py
+++ app.py
@@ -5,24 +5,26 @@ import dash_html_components as html
 import os
 
 ###### Set up variables
-list_of_choices=['happy', 'angry', 'sad']
+list_of_choices=['angry', 'happy', 'sad']
 githublink = 'https://github.com/astever31/201-chuck-norris-callback'
-#image1='happy-pika.jpg'
-list_of_images=['happy-pika.jpg', 'angry-pika.jpg', 'sad-pika.jpg']
+#image1='mad-pika.jpg'
+list_of_images=['mad-pika.jpg', 'happy-pika.jpg', 'sad-pika.jpg']
 heading1='The various emotions of Pikachu'
+mydict={list_of_choices[i]:list_of_images[i] for i in range(len(list_of_choices))}
+tabtitle='Pikachu'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='Pikachu'
+app.title=tabtitle
 
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
     dcc.Dropdown(id='your-input-here',
-                options=[{'label': i, 'value': n} for i in list_of_choices for n in list_of_images],
-                value=list_of_choices,
+                options=[{'label': i, 'value': n} for i, n in mydict.items()],
+                value=list_of_images[0],
                 style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
@@ -36,8 +38,7 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return html.Img(src=app.get_asset_url(whatever_you_chose), style={'width': 'auto', 'height': '10%'}),
-f'You made Pikachu {whatever_you_chose}!'
+    return html.Img(src=app.get_asset_url(whatever_you_chose), style={'width': 'auto', 'height': '10%'}) #f'Chuck Norris will now execute you with a {whatever_you_chose}.'
 
 
 ######### Run the app #########
