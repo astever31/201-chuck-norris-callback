8d729af6d804350413bf6c3ea0d9e2deab594745
Analese Steverson Pugh <analese_steversonpugh@intuit.com>
2022-07-21 01:24:44 +0000

    getting there, one more update to app.py

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ app.title='Chuck'
 app.layout = html.Div([
     html.H2(heading1),
     dcc.Dropdown(id='your-input-here',
-                options=[{'label': i, 'value': n} for i in list_of_choices for n in list_of_images],
+                options=[
+                {'label':list_of_options[0], 'value':list_of_images[0]},
+                {'label':list_of_options[1], 'value':list_of_images[1]},
+                {'label':list_of_options[2], 'value':list_of_images[2]},
+                {'label':list_of_options[3], 'value':list_of_images[3]},
+                ],
                 value=list_of_images[0],
                 style={'width': '500px'}),
     html.Br(),
@@ -36,7 +41,8 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return html.Img(src=app.get_asset_url(whatever_you_chose), style={'width': 'auto', 'height': '10%'}), f'Chuck Norris will now execute you with a {whatever_you_chose}.'
+    return html.Img(src=app.get_asset_url(whatever_you_chose), style={'width': 'auto', 'height': '10%'}), 
+f'Chuck Norris will now execute you with a {whatever_you_chose}.'
 
 
 ######### Run the app #########
