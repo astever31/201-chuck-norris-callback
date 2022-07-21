fc92d92198accca7c986e05fd6fbde6a257f0bf2
Analese Steverson Pugh <analese_steversonpugh@intuit.com>
2022-07-21 19:40:50 +0000

    updated app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import dash_html_components as html
 import os
 
 ###### Set up variables
-list_of_choices=['angry', 'happy', 'sad']
+list_of_choices=['happy', 'angry', 'sad']
 githublink = 'https://github.com/astever31/201-chuck-norris-callback'
-image1='happy-pika.jpg'
-list_of_images=['mad-pika.jpg', 'happy-pika.jpg', 'sad-pika.jpg']
+#image1='happy-pika.jpg'
+list_of_images=['happy-pika.jpg', 'mad-pika.jpg', 'sad-pika.jpg']
 heading1='The various emotions of Pikachu'
 #mydict={list_of_choices[i]:list_of_images[i] for i in range(len(list_of_choices))}
 tabtitle='Pikachu'
@@ -22,10 +22,10 @@ app.title=tabtitle
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
-    html.Img(id='image-output', src=app.get_asset_url(image1)),
+    html.Img(id='image-output', src=app.get_asset_url(list_of_images[0])),
     dcc.Dropdown(id='your-input-here',
                 options=[{'label': list_of_choices[i], 'value': i} for i in range(len(list_of_choices))],
-                value='angry',#list_of_choices[1],
+                value=list_of_choices[0],
                 style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
@@ -44,7 +44,7 @@ def display_value(whatever_you_chose):
 @app.callback(dash.dependencies.Output('image-output', 'src'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return app.get_asset_url(list_of_images[whatever_you_chose])#, style={'width': 'auto', 'height': '10%'}
+    return app.get_asset_url(list_of_images[whatever_you_chose]), style={'width': 'auto', 'height': '10%'}
 
 ######### Run the app #########
 if __name__ == '__main__':
